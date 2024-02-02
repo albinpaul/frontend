8f5202fafd37496996cad5ec905e4ff9c10130be
Albin Paul <albinpaul.ks@gmail.com>
2024-02-02 23:18:37 +0530

    changing backend

--- src/creds/backend_cred.tsx
+++ src/creds/backend_cred.tsx
@@ -1,2 +1,3 @@
 // export const backend_url = "http://localhost:3000"
-export const backend_url = "http://ec2-3-110-194-38.ap-south-1.compute.amazonaws.com:3000"
+// export const backend_url = "http://ec2-3-110-194-38.ap-south-1.compute.amazonaws.com:3000"
+export const backend_url = "https://memory-backend-o6gn.onrender.com/"
